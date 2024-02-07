450daa61313c83d2a52a60df932176290a58f319
yara2 <yara200359@gmail.com>
2024-02-08 00:09:50 +0200

    firist_project

---
+++ Build_small_calculater.py
@@ -0,0 +1,13 @@
+num1=input("Please enter the frist number: ")
+num2=input("Please enter the secound number: ")
+op=input("Select the operation that you want to do in this two number: ")
+if op=='+':
+    print(int(num1)+int(num2))
+elif op=='-':
+    print(int(num1)-int (num2))
+elif op=='*':
+    print(int(num1)*int(num2))
+elif op=='/':
+    print(int(num1)/int(num2))
+elif op=='^':
+    print(int(num1)**int(num2))
